709981f42490ffe73d85f414229328862c2acfd9
luisfe97 <luisfelipegomezr@hotmail.com>
2022-09-26 19:40:39 -0500

    Distancia

--- Assets/Scripts/ExampleData.cs
+++ Assets/Scripts/ExampleData.cs
@@ -7,8 +7,13 @@ public class ExampleData
 	
 	public string username;
 	public int levelfail;
+	public float porcentaje1;
 	public int levelfail2;
+	public float porcentaje2;
 	public int levelfail3;
+	public float porcentaje3;
 	public int levelfail4;
+	public float porcentaje4;
 	public int levelfail5;
+	public float porcentaje5;
 }
